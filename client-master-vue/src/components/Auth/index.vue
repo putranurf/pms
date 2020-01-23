49b03002f62e23bb1c46601610f0e5698d32f494
putranurf <putranf@gmail.com>
2020-01-23 09:25:49 +0700

    Mengubah title dan nama form di auth

--- client-master-vue/src/components/Auth/index.vue
+++ client-master-vue/src/components/Auth/index.vue
@@ -6,7 +6,7 @@
           <v-flex xs12 sm8 md4>
             <v-card class="elevation-12">
               <v-toolbar dark color="primary">
-                <v-toolbar-title>PMS DEVELOPMENT - LOGIN</v-toolbar-title>
+                <v-toolbar-title>PMS - LOGIN</v-toolbar-title>
                 <v-spacer></v-spacer>
                 <!-- <v-tooltip bottom>
                   <template v-slot:activator="{ on }">
